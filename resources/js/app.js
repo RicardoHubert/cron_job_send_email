a65cb56699a60e6c7705ce65ef7ed5096e07cc26
RicardoHubert <ricardohub681@gmail.com>
2023-10-23 13:59:02 +0700

    [Config] Setup vue component requirments

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import NewArrivals from './components/NewArrivals.vue';
+
+import swal from 'sweetalert2';
+window.swal = swal;
+
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+
+Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker);
+
+Vue.component('new-arrivals', NewArrivals);
+
+
+const app = new Vue({
+	el: '#app'
+});
