4f97b21db9f3b3b5b4bc83d352145200e24a9ea4
RicardoHubert <ricardohub681@gmail.com>
2023-10-23 14:09:52 +0700

    [Feat] Create layout index with vue

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
-<?php
+    <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
+    use app\Http\Controller\MessageController;  
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/get_users', 'MessageController@getUsers');
+    Route::get('/get_messages', 'MessageController@getMessages');
+    Route::post('notifications', 'MessageController@sendMail');
+
